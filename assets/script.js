58a8e31387fd0b767259d3155392372a36cc9118
[Keval Patel] <[khp90611@gmail.com]>
2023-08-05 18:23:45 -0400

    Created event listener on search button click and input the search into the city name. Fetch request gets the first name of the city

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,24 @@
-
+let cityInput = document.getElementById('city-input');
+let column1Container = document.getElementById('column-1');
+let cityDisplay = document.getElementById('city-name')
 //-----------------------Fetch using Open Weather API for CITY NAME 
 
-// fetch('api.openweathermap.org/data/2.5/forecast?q=London&limit=5&appid=cadb2b964c2c86cd32342e930ae11b93'){
-//     .then (response =>)
-// }
 
+//--------------------Event Listener for search then uses API to get information on that city//
+column1Container.addEventListener('click', function search(event) {
+    if (event.target === document.querySelector('button')) {
+        console.log(event.target)
+        cityDisplay.textContent = cityInput.value
+
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityInput.value}}&limit=5&appid=cadb2b964c2c86cd32342e930ae11b93`)
+            .then(response => response.json())
+            .then(citiesFound => {
+                let firstCity = citiesFound[0].name
+                console.log(firstCity)
+                
+            })
+    }
+})
 //--------------------------Input saved as a button--------------------//
 
 
@@ -15,3 +29,6 @@
 
 //--------------------------Create element ----------------------------//
 
+function createFiveDayForecastCards() {
+    
+}
