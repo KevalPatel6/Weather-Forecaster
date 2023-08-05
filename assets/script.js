1aed4b21b9c0d0df98411768c989fb88514a32a9
[Keval Patel] <[khp90611@gmail.com]>
2023-08-05 16:50:59 -0400

    filled out HTML a bit for visual representation of where things go. Used bootstrap to lay out the content in aappropriate places. Now need to work on javascript to load information

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+
+//-----------------------Fetch using Open Weather API for CITY NAME 
+
+// fetch('api.openweathermap.org/data/2.5/forecast?q=London&limit=5&appid=cadb2b964c2c86cd32342e930ae11b93'){
+//     .then (response =>)
+// }
+
+//--------------------------Input saved as a button--------------------//
+
+
+//-----------------------Create buttons based on inputs-------------------//
+
+//-------------------------------------------------------------------//
+
+
+//--------------------------Create element ----------------------------//
+
