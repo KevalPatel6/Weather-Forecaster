a1065d699b9d20101e848f0038aff3c47994cc88
[Keval Patel] <[khp90611@gmail.com]>
2023-08-07 21:39:31 -0400

    fixed local storage, fixed card spacing so it is horizontal, buttons link to the information of the city. Used CSS to order the list in reverse order so most recent is at the top

--- assets/script.js
+++ assets/script.js
@@ -4,128 +4,137 @@ let cityDisplay = document.getElementById('city-name');
 let previousCitiesList = document.getElementById('previous-cities-search');
 let cityDataList = document.querySelector('#city-weather-data');
 let allCardsContainer = document.getElementById('all-cards-container')
-let previousSearchArray = [];
-localStorage.setItem('cities', previousSearchArray)
+let previousSearchArray = JSON.parse(localStorage.getItem('cities')) || [];
+// localStorage.setItem('cities', previousSearchArray)
 //-----------------------Fetch using Open Weather API for CITY NAME 
 
 
-//--------------------Event Listener for search then uses API to get information on that city//
-column1Container.addEventListener('click', function search(event) {
+for (let i = 0; i < previousSearchArray.length; i++) {
+    createButtons(previousSearchArray[i])
+
+}
+
+column1Container.addEventListener('click', search)
+
+
+function search(event) {
     if (event.target === document.querySelector('#search-button')) {
         event.preventDefault();
-        createButtonsAndStore(cityInput.value);
-
-       
-
-        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityInput.value}}&limit=5&appid=91ea62a2bafa882dacaf79bb891172d8`)
-            .then(response => response.json())
-            .then(citiesFound => {
-
-
-                //    if(citiesFound.length<1){
-                //     return console.log("Broken")
-                //    }
+        createButtons(cityInput.value);
+        pushAndSave(cityInput.value);
+        fetchData(cityInput.value);
 
 
 
-                let firstCity = citiesFound[0]
-
-                
-                return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=91ea62a2bafa882dacaf79bb891172d8&units=imperial`)
 
 
-            })
-            .then(response => response.json())
-            .then(weatherData => {
-               
-                cityDisplay.textContent = cityInput.value + "    (" + dayjs().format('M/DD/YYYY') + ")" + "  "
 
-                let iconNum = weatherData.weather[0].icon
-                let iconDesc = weatherData.weather[0].description
-                let temp = weatherData.main.temp
-                let windSpeed = weatherData.wind.speed
-                let humidity = weatherData.main.humidity
-                iconRender(iconNum, iconDesc, cityDisplay)
+    }
+}
 
-            
-                //-------------------Dont need above console log, remember to delete----------------------//
-                emptyData(cityDataList);
-                createWeatherDataElements(temp, windSpeed, humidity);
 
-                emptyData(allCardsContainer)
-                
-
-                return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=91ea62a2bafa882dacaf79bb891172d8&units=imperial`)
-            })
-             
-        
-            .then(response => response.json())
-            .then(forecastData =>{
-              
-               
-
-                createForecastCards()
-
-               
-                renderForecastInfo(forecastData)
-               
-                
-                
-
-            })
-        //-------------------------------------Add button for each search------------------------------//
-
-function createButtonsAndStore(x){
+function createButtons(x) {
     let buttonEl = document.createElement('button')
     buttonEl.textContent = x
-    buttonEl.className = `w-100 btn-md btn-secondary my-3`
+    buttonEl.className = `w-100 btn-lg btn-secondary my-3 rounded-1 border-0 created-buttons`
     buttonEl.id = x
     buttonEl.setAttribute("type", "button")
     previousCitiesList.append(buttonEl)
 
-   
+    buttonEl.addEventListener("click", function (event) {
+        fetchData(event.target.textContent)
 
-    localStorage.setItem('cities', JSON.stringify(buttonEl.textContent))
-    
+    })
 
+}
+
+function pushAndSave(x) {
+
+    previousSearchArray.push(x)
+
+    localStorage.setItem('cities', JSON.stringify(previousSearchArray))
+
+    if (previousSearchArray.length > 8) {
+
+        previousSearchArray.splice(8, 1)
+
+    }
 
 }
 
 
-//-------------------------------------Remove button from list if it exceeds a certain length-------------//
-    
-//--------------------------------------Local Storage for buttons-------------------------------------//
+function fetchData(input) {
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${input}}&limit=5&appid=91ea62a2bafa882dacaf79bb891172d8`)
+        .then(response => response.json())
+        .then(citiesFound => {
+
+            let firstCity = citiesFound[0]
+
+            return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=91ea62a2bafa882dacaf79bb891172d8&units=imperial`)
+
+        })
+        .then(response => response.json())
+        .then(weatherData => {
+            console.log(weatherData)
+            cityDisplay.textContent = input + ", " + weatherData.sys.country + "     (" + dayjs().format('M/DD/YYYY') + ")" + "  "
+
+            let iconNum = weatherData.weather[0].icon
+            let iconDesc = weatherData.weather[0].description
+            let temp = weatherData.main.temp
+            let windSpeed = weatherData.wind.speed
+            let humidity = weatherData.main.humidity
+            
+            iconRender(iconNum, iconDesc, cityDisplay)
+            emptyData(cityDataList);
+            createWeatherDataElements(temp, windSpeed, humidity);
+            emptyData(allCardsContainer)
+
+
+            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${weatherData.coord.lat}&lon=${weatherData.coord.lon}&appid=91ea62a2bafa882dacaf79bb891172d8&units=imperial`)
+        })
+
+
+        .then(response => response.json())
+        .then(forecastData => {
 
 
 
+            createForecastCards()
+
+
+            renderForecastInfo(forecastData)
+
+
+
+
+        })
+}
+
+
+function iconRender(iconID, iconDesc, whereAppend) {
+
+    let imgEl = document.createElement('img')
+    imgEl.setAttribute('src', `https://openweathermap.org/img/wn/${iconID}@2x.png`)
+    imgEl.setAttribute('alt', iconDesc)
+    whereAppend.append(imgEl)
 
 }
-})
 
-//------------------------------------Functions----------------------------------------------//
-// function loadFromStorage() {
-//                     //------Can make this function expression-----//
-//     let previousSearch = parse.JSON(localStorage.getItem() || [])
-
-    
-
-
-
-
-function renderForecastInfo(forecastData){
+function renderForecastInfo(forecastData) {
     for (let i = 1; i < 6; i++) {
-        
-        let dayInfo = forecastData.list[(8*i) -2]
+
+        let dayInfo = forecastData.list[(8 * i) - 2]
         let cardDate = document.querySelector(`.card-header-${i}`)
         let cardIcon = document.querySelector(`.icon-${i}`)
         let cardTemp = document.querySelector(`.temp-${i}`)
         let cardWind = document.querySelector(`.wind-${i}`)
         let cardHumidity = document.querySelector(`.humidity-${i}`)
 
-        cardDate.textContent = dayjs(dayInfo.dt_txt).format('M/DD/YY  h:mm A')
+        cardDate.textContent = dayjs(dayInfo.dt_txt).format('M/DD/YY')
 
-        cardIcon.textContent = `Icon`
-        iconRender(dayInfo.weather[0].icon, dayInfo.weather[0].description,cardIcon)
+        cardIcon.textContent = ``
+        iconRender(dayInfo.weather[0].icon, dayInfo.weather[0].description, cardIcon)
+        cardIcon.className = "text-center"
         cardTemp.textContent = "Temp: " + dayInfo.main.temp + "°F"
         cardWind.textContent = "Wind: " + dayInfo.wind.speed + " MPH"
         cardHumidity.textContent = "Humidity: " + dayInfo.main.humidity + "%"
@@ -137,20 +146,19 @@ function renderForecastInfo(forecastData){
 
 
 
-
 //-----------------------Create fetch for buttons based on inputs-------------------//
-    // let selectedButton = document.querySelector(`#${cityInput.value}`)
-    // column1Container.addEventListener('click', function(event){
-    //     if(event.target===selectedButton){
-           
-    // fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${selectedButton}}&limit=5&appid=91ea62a2bafa882dacaf79bb891172d8`)
-    //     .then(response => response.json())
-    //     .then(citiesFoundByButton =>{
-    //         console.log(citiesFoundByButton)
+// let selectedButton = document.querySelector(`#${cityInput.value}`)
+// column1Container.addEventListener('click', function(event){
+//     if(event.target===selectedButton){
 
-    //     })
-    //     }
-    // })
+// fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${selectedButton}}&limit=5&appid=91ea62a2bafa882dacaf79bb891172d8`)
+//     .then(response => response.json())
+//     .then(citiesFoundByButton =>{
+//         console.log(citiesFoundByButton)
+
+//     })
+//     }
+// })
 
 
 
@@ -172,7 +180,7 @@ function createForecastCards() {
 
 
         let cardBox = document.createElement('section')
-        cardBox.className = ` col card-${i}`
+        cardBox.className = `card-${i}`
         cardBox.setAttribute('style', 'width: 18rem;')
         allCardsContainer.append(cardBox)
 
@@ -231,11 +239,3 @@ function createWeatherDataElements(x, y, z) {
 
 }
 
-function iconRender(iconID, iconDesc,whereAppend) {
-
-    let imgEl = document.createElement('img')
-    imgEl.setAttribute('src', `https://openweathermap.org/img/wn/${iconID}@2x.png`)
-    imgEl.setAttribute('alt', iconDesc)
-    whereAppend.append(imgEl)
-
-}
